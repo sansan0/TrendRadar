368f750b5f08bb1b086b871fde1b9b0bda9d9c7b
Nicky Lao <giscafer@outlook.com>
2025-11-16 17:31:08 +0800

    Fix formatting of update time and version info

--- main.py
+++ main.py
@@ -3293,9 +3293,10 @@ def render_feishu_content(
     text_content += (
         f"\n\n<font color='grey'>更新时间：{now.strftime('%Y-%m-%d %H:%M:%S')}</font>"
     )
-
+"""
     if update_info:
         text_content += f"\n<font color='grey'>TrendRadar 发现新版本 {update_info['remote_version']}，当前 {update_info['current_version']}</font>"
+"""
 
     return text_content
 
@@ -3386,10 +3387,10 @@ def render_dingtalk_content(
             text_content += f"  • **{id_value}**\n"
 
     text_content += f"\n\n> 更新时间：{now.strftime('%Y-%m-%d %H:%M:%S')}"
-
+"""
     if update_info:
         text_content += f"\n> TrendRadar 发现新版本 **{update_info['remote_version']}**，当前 **{update_info['current_version']}**"
-
+"""
     return text_content
 
 
